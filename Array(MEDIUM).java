fec9f8fdd5696e14f0389956170264aa2c79bddb
Jeet Das <101659605+itsjeetz@users.noreply.github.com>
2024-02-07 11:32:14 +0530

    Update Array(MEDIUM).java

--- Array(MEDIUM).java
+++ Array(MEDIUM).java
@@ -338,3 +338,40 @@ public class Solution {
     }
 }
 
+---------------------------------------------------------------------------------------------------------
+//Q8) Set Matrix Zero--If an element is zero mark all the elements in that row and column as zero.
+//Time : O(2*M*N)  Space : O(M)+O(N) --> not optimal 
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int i,j;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int row[]=new int[n];
+        int col[]=new int[m];
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<m;j++)
+            {
+                if(row[i]==1 || col[j]==1)
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+    }
+}
+
+//Time : O()  Space : O()
