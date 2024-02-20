4bedc55e95b5a735de5a764126b3116d1de45552
Jeet Das <101659605+itsjeetz@users.noreply.github.com>
2024-02-21 01:59:56 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -85,7 +85,100 @@ class Solution {
 }
 
 
-//Q4) FLoor/Ceil in sorted array.
+//Q4) Search in Rotated Sorted Array(Unique Elements)
+
+class Solution {
+    public int search(int[] nums, int target) 
+    {
+        int n=nums.length;
+        int low=0,high=n-1,mid;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[low]<=nums[mid] )
+            {
+                if(nums[low]<=target && nums[mid]>=target)
+                    high=mid-1;
+                else
+                    low=mid+1;
+            }
+            else
+            {
+                if(nums[high]>=target && nums[mid]<=target)
+                    low=mid+1;
+                else
+                    high=mid-1;
+            }
+        }
+        return -1;      
+    }
+}
+
+//Q5) Search in Rotated Sorted Array(Elements are not distinct)
+
+class Solution {
+    public boolean search(int[] nums, int target) 
+    {
+        int n=nums.length;
+       int low=0,high=n-1,mid;
+       while(low<=high)
+       {
+           mid=(low+high)/2;
+           if(nums[mid]==target)
+           {
+               return true;
+           }
+           if(nums[mid]==nums[low] && nums[mid]==nums[high])
+           {
+               low++;
+               high--;
+               continue;
+           }
+
+           if(nums[low]<=nums[mid])
+           {
+               if(target>=nums[low] && nums[mid]>=target)
+                    high=mid-1;
+                else
+                    low=mid+1;
+           }
+           else
+           {
+               if(target>=nums[mid] && target<=nums[high])
+                    low=mid+1;
+                else
+                    high=mid-1;
+           }
+       }
+       return false;
+    }
+}
+
+//Q6) Find Minimum is rotated sorted array
+
+public class Solution {
+    public static int findMin(int []arr) {
+        int high=arr.length-1,low=0,mid;
+        int ans=Integer.MAX_VALUE;
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+            if(arr[low]<=arr[mid])
+            {
+                ans=(ans<arr[low])?ans:arr[low];
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+                ans=(ans<=arr[mid])?ans:arr[mid];
+            }
+        }
+        return ans;
+    }
+}
 
 
 
